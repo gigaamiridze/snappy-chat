a07304969f62e5ee81db37e75d5d4fde318d54f0
justtfelix <justttfelix@gmail.com>
2023-05-29 23:43:57 +0400

    [Server] Added cors and json body parser middlewares

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,11 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 import { PORT } from './config';
 
 const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
