b1ec5b7674169cd6c9693da0e6f50daef41e39fc
justtfelix <justttfelix@gmail.com>
2023-05-29 23:35:22 +0400

    [Server] Created server using express

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,12 @@
-console.log('Hello, World!');
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+const port: number = 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
