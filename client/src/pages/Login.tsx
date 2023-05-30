72fcd59dd5df71b8ac4ed1ccfe004da5dfd1c482
justtfelix <justttfelix@gmail.com>
2023-05-30 23:41:23 +0400

    [Client] Added login page content

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,27 @@
+import { AuthContainer, AuthForm, AuthInput, AuthButton } from '../components';
+import { Brand, ToastNote } from '../layouts';
+
 function Login() {
   return (
-    <div>Login</div>
+    <AuthContainer>
+      <Brand />
+      <AuthForm>
+        <AuthInput
+          type='text'
+          name='username'
+          id='username'
+          placeholder='Username'
+        />
+        <AuthInput
+          type='password'
+          name='password'
+          id='password'
+          placeholder='Password'
+        />
+        <AuthButton>login</AuthButton>
+      </AuthForm>
+      <ToastNote />
+    </AuthContainer>
   )
 }
 
