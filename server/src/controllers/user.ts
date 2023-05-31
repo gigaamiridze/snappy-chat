128afdbd7cb48b5e3e972b74797f5a16e69c8f23
justtfelix <justttfelix@gmail.com>
2023-05-31 08:41:48 +0400

    [Server] Added login route and its controller

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -35,3 +35,29 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
     next(error);
   }
 }
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({
+        status: 'fail',
+        message: 'Incorrect username or password',
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        status: 'fail',
+        message: 'Incorrect username or password',
+      });
+    }
+
+    return res.status(200).json({ status: 'success', user });
+  } catch (error) {
+    next(error);
+  }
+}
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import { register } from '../controllers';
+import { register, login } from '../controllers';
 
 export const userRouter = Router();
 
 userRouter.post('/register', register);
+userRouter.post('/login', login);
